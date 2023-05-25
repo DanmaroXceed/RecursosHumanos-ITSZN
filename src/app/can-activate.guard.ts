2a7febd851f4370ee2f1237cf3724ad85a763ea8
Daniel Martinez <daniel_martinez1103@outlook.com>
2023-05-25 11:54:21 -0600

    Packages actualizados. Se añadio CookieService

--- src/app/can-activate.guard.ts
+++ src/app/can-activate.guard.ts
@@ -3,14 +3,14 @@
 // Permite verificar si el usuario esta logeado para dar acceso a rutas, sino lo manda a iniciar sesion
 
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './core/servicios/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CanActivateGuard implements CanActivate {
+export class CanActivateGuard  {
 
   constructor(
     private router : Router,
