d27c7cf8c989bd97634aa9c878baf5b81a96abf1
Abraham Esquivel Salas <abraham.esquivel@gmail.com>
2023-06-19 13:36:04 -0600

    Crud StudiesProgram

--- src/app/studies-program/studies-program.component.ts
+++ src/app/studies-program/studies-program.component.ts
@@ -1,29 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, OnChanges, OnDestroy, OnInit, Output} from '@angular/core';
 import {StudiesProgram} from "../../models/StudiesProgram";
 import {StudiesProgramService} from "./studies-program.service";
 import {HttpErrorResponse} from "@angular/common/http";
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-studies-program',
   templateUrl: './studies-program.component.html',
   styleUrls: ['./studies-program.component.css']
 })
-export class StudiesProgramComponent {
+export class StudiesProgramComponent implements OnInit{
 
   public studiesprograms: StudiesProgram[]=[];
   public spToCreate: StudiesProgram = {} as StudiesProgram;
   public isModalVisible = false;
   spform : FormGroup;
+  //subscription : Subscription=Subscription.EMPTY;
 
   ngOnInit(): void{
       this.isModalVisible=false;
       this.getAllStudiesPrograms();
   }
 
+  get f(){
+    return this.spform.controls;
+  }
+
   constructor(private studiesProgramService: StudiesProgramService, private formBuilder:FormBuilder) {
     this.spform = this.formBuilder.group({
-
+        studiesProgramName:['', Validators.required]
     });
   }
 
@@ -38,14 +44,18 @@ export class StudiesProgramComponent {
     this.studiesProgramService.addStudiesProgram(sp).subscribe(
     (data:any)=>{this.spToCreate = data;}
     );
-    this.getAllStudiesPrograms();
   }
 
     onEdit(sp: StudiesProgram) {
-        
+      //this.spform.patchValue({studiesProgramName: sp.studiesProgramName});
+      this.spform.get('studiesProgramName')?.setValue(sp.studiesProgramName);
+      this.isModalVisible = true;
     }
 
-  onDelete(sp: StudiesProgram) {
-    
+  onDelete(id: number) {
+    this.studiesProgramService.deleteStudiesProgram(id).subscribe(
+      (data:any)=>{console.log(data);}
+    );
   }
+
 }
--- src/app/studies-program/studies-program.service.ts
+++ src/app/studies-program/studies-program.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
-import {User} from "../../models/User";
-import {Observable} from "rxjs";
+import {Observable, Subject} from "rxjs";
 import {StudiesProgram} from "../../models/StudiesProgram";
+//import {tap} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -11,23 +11,34 @@ import {StudiesProgram} from "../../models/StudiesProgram";
 export class StudiesProgramService {
 
   private apiServerUrl = environment.apiBaseUrl;
+ // private _refresh$ = new Subject<void>();
 
   constructor(private http: HttpClient) { }
-
+/*
+  get refresh(){
+    return this._refresh$;
+  }
+*/
   public getAllStudiesProgram(): Observable<StudiesProgram[]>{
     return this.http.get<StudiesProgram[]>(`${this.apiServerUrl}/api/v1/registration/studiesprogram/all`);
   }
 
   public addStudiesProgram(sp: StudiesProgram): Observable<StudiesProgram>{
     return this.http.post<StudiesProgram>(`${this.apiServerUrl}/api/v1/registration/studiesprogram/add`, sp);
+        /*
+        .pipe(
+        tap(()=>{
+          this._refresh$.next();
+        })
+         */
   }
 /*
   public updateStudiesProgram(sp: StudiesProgram): Observable<StudiesProgram>{
     return this.http.put<StudiesProgram>(`${this.apiServerUrl}/api/v1/registration/studiesprogram/update`, sp);
   }
 */
-  public deleteStudiesProgram(id: number): Observable<void>{
-    return this.http.delete<void>(`${this.apiServerUrl}/api/v1/registration/studiesprogram/delete/${id}`);
+  public deleteStudiesProgram(id: number): Observable<boolean>{
+    return this.http.delete<boolean>(`${this.apiServerUrl}/api/v1/registration/studiesprogram/delete/${id}`);
   }
 
 }
